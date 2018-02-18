6a330e4b8e9456e127ee2b72b5b1c5c85c007813
Treasure <treasure.rose@gmail.com>
2018-02-18 12:59:30 -0800

    stage 1 complete

--- app.js
+++ app.js
@@ -1,27 +1,17 @@
-// 1. First, let's build out some basic functionality with some mock data
-// 2. Write some quick CSS to make the data look like a card. 
-// 3. Now we want to toggle showing the front and back sides of the cards. 
-// If the user clicks on the card and the front is showing, we want to show the 
-// back. If the user clicks on the card and back is showing, we can to show the
-// front. 
-
 const cards = [
   {
-    'card_id': 1,
-    'question': 'This is the front side of the card.',
-    'answer': 'This is the back side of the card.',
+    'front': 'This is the front side of the card.',
+    'back': 'This is the back side of the card.',
     'active': false
   },
   {
-    'card_id': 2,
-    'question': 'This is card 2',
-    'answer': 'This is the back side of card 1.',
+    'front': 'This is card 2',
+    'back': 'This is the back side of card 2.',
     'active': false
   },
   {
-    'card_id': 3,
-    'question': 'This is card 3',
-    'answer': 'This is the back side of card 3.',
+    'front': 'This is card 3',
+    'back': 'This is the back side of card 3.',
     'active': false
   },
 ]
@@ -30,10 +20,21 @@ new Vue({
   el: '#cardList',
   data: {
     cards: cards,
+    cardFront: '',
+    cardBack: ''
   },
   methods: {
     toggleCard: function(card){
       card.active ? card.active = false : card.active = true;
-    }
+    },
+    addNew: function(){
+      this.cards.push({
+        'front': this.cardFront,
+        'back': this.cardBack,
+        'active': false
+      });
+      this.cardFront = '';
+      this.cardBack = '';
+    },
   }
 });
